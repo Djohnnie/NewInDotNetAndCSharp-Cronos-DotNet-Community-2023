e7a1c8ff74b425feb2d7ac01fd44de74cc5c7936
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2023-04-25 22:59:00 +0200

    Minor tweaks

--- DotNetAndCSharp.CSharp12.PrimaryConstructors/Program.cs
+++ DotNetAndCSharp.CSharp12.PrimaryConstructors/Program.cs
@@ -1,9 +1,4 @@
 ﻿
-
-
-
-
-
 public class Student(string name, string firstName, byte age)
 {
     public Student(string name, string firstName) : this(name, firstName, 18)
@@ -14,4 +9,6 @@ public class Student(string name, string firstName, byte age)
     public string Name => name;
 
     public string FirstName => firstName;
+
+    public byte Age => age;
 }
--- DotNetAndCSharp.CSharp12.UsingDirectivesForAdditionalTypes/Program.cs
+++ DotNetAndCSharp.CSharp12.UsingDirectivesForAdditionalTypes/Program.cs
@@ -1,10 +1,22 @@
-﻿
-
+﻿using NullableGuid = System.Guid?;
+using Ints = int[];
 using ComplexNumber = (decimal Real, decimal Imaginary);
 
 
 
-var complexNumber = new ComplexNumber { Real = 1.0m, Imaginary = 2.0m };
+NullableGuid guid = Guid.Empty;
+guid = null;
 
 
-Console.WriteLine($"{complexNumber.Real} + {complexNumber.Imaginary}i");
+var doStuff = (Ints i) => Console.WriteLine(i.Length);
+doStuff(new[] { 1, 2, 3 });
+
+
+var complexNumber = new ComplexNumber
+{
+    Real = 1.0m,
+    Imaginary = 2.0m
+};
+
+Console.WriteLine(
+    $"{complexNumber.Real} + {complexNumber.Imaginary}i");
